2c7d22e15097aec47927c90584aca246104c0dc1
キノ0510 <kinont0510@gmail.com>
2024-12-15 18:06:54 +0900

    add:components/layouts

---
+++ frontend/components/layouts/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="w-full text-center text-mint bg-customGreen p-1">
+            <p>© 2024 N E N R I N. All rights reserved.</p>
+        </div>
+    )
+}
+
+export default Footer;
